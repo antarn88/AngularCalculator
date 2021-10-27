1a5b42e08cbb4ddbb1d269d07718d2bd1c60f972
Arnold <antaloczi.arnold@gmail.com>
2021-10-27 20:16:01 +0200

    Completed app

--- src/app/widget/button/button.component.ts
+++ src/app/widget/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -9,10 +9,15 @@ export class ButtonComponent implements OnInit {
 
   @Input() label = '';
   @Input() wide = false;
+  @Output() pressBtn: EventEmitter<string> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(value: string): void {
+    this.pressBtn.emit(value);
   }
 
 }
--- src/app/widget/calculator/calculator.component.ts
+++ src/app/widget/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MemoryService } from 'src/app/service/memory.service';
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  currentClickValue = '';
+
+  constructor(
+    public memory: MemoryService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  receiveClick(value: string): void {
+    this.currentClickValue = value;
+
+    if (value === '=') {
+      this.equal();
+    } else if (value === 'C') {
+      this.memory.buffer = '';
+    } else if (value === 'Del') {
+      this.memory.buffer = this.memory.buffer.substring(0, this.memory.buffer.length - 1);
+    } else if (value === '+/-') {
+      if (typeof parseFloat(this.memory.buffer) === 'number' && this.memory.buffer.length !== 0) {
+        this.memory.buffer = `-${this.memory.buffer}`;
+      }
+    }
+    else {
+      const lastChar = this.memory.buffer[this.memory.buffer.length - 1];
+      if (!(lastChar === value && (lastChar === '+' || lastChar === '-' || lastChar === '.'))) {
+        this.memory.buffer += value;
+      }
+    }
+  }
+
+  equal(): void {
+    let result = 0;
+    let numbersStr = '';
+
+    if (!this.memory.buffer.startsWith('-')) {
+      numbersStr = this.memory.buffer.split('+').join(', ').split('-').join(', ');
+    } else {
+      numbersStr = this.memory.buffer.split('+').join(', ').split('-').join(', ').replace(', ', '-');
+    }
+
+    const numbersArray = numbersStr.split(', ').map((number: string) => parseFloat(number));
+    const signsArray = Array.from(this.memory.buffer).filter(char => char === '+' || char === '-');
+    this.memory.buffer[0] === '-' ? signsArray.shift() : null;
+
+    numbersArray.map((number, index) => {
+      if (signsArray[index] === '+') !index ? result = number + numbersArray[index + 1] : result += numbersArray[index + 1];
+      else if (signsArray[index] === '-') !index ? result = number - numbersArray[index + 1] : result -= numbersArray[index + 1];
+    });
+
+    this.memory.buffer = result.toString();
   }
 
 }
